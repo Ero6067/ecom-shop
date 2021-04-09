720bb2aeee2cebdec98a21d3dbea694dd7215046
Kris Lafrance <kris.lafrance6067@gmail.com>
2021-04-09 02:12:53 -0700

    fixed issue where price wasn't being uploaded to mongodb

--- backend/seeder.js
+++ backend/seeder.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import users from './data/users.js'
 import products from './data/products.js'
-import User from './models/userModel.js' 
-import Product from './models/productModel.js' 
-import Order from './models/orderModel.js' 
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
 import connectDB from './config/db.js'
 
 dotenv.config()
@@ -13,45 +13,47 @@ dotenv.config()
 connectDB()
 
 const importData = async () => {
-    try {
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
 
-        const createdUsers = await User.insertMany(users)
+    const createdUsers = await User.insertMany(users)
 
-        const adminUser = createdUsers[0]._id
+    const adminUser = createdUsers[0]._id
 
-        const sampleProducts = products.map(product => {
-            return { ...product, user: adminUser }
-        })
+    //adds the admin user to each of the products user field
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser }
+    })
 
-        await Product.insertMany(sampleProducts)
+    await Product.insertMany(sampleProducts)
 
-        console.error('Data Imported'.green.inverse)
-        process.exit()
-    } catch (error) {
-        console.log(`${error}`.red.inverse)
-        process.exit(1)
-    }
+    console.log('Data Imported'.green.inverse)
+    console.log(sampleProducts[0].price)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
 }
 
 const destroyData = async () => {
-    try {
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
 
-        console.error('Data Destroyed'.red.inverse)
-        process.exit()
-    } catch (error) {
-        console.log(`${error}`.red.inverse)
-        process.exit(1)
-    }
+    console.error('Data Destroyed'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.log(`${error}`.red.inverse)
+    process.exit(1)
+  }
 }
 
-if(process.argv[2]=== '-d') {
-    destroyData() 
+if (process.argv[2] === '-d') {
+  destroyData()
 } else {
-    importData()
+  importData()
 }
