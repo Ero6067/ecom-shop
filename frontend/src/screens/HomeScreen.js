480e116b76516922cc35d51934d1c84e0cf3ac1e
Kris Lafrance <kris.lafrance6067@gmail.com>
2021-04-11 00:55:11 -0700

    updated homescreen with redux using dispatch and selector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
-import Product from '../components/Product';
-import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-	const [products, setProducts] = useState([]);
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
 
-	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/products');
+  return (
+    <>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <h2>Loading..</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col
+              className="align-items-stretch d-flex"
+              key={product._id}
+              sm={12}
+              med={6}
+              lg={4}
+              xl={3}
+            >
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  )
+}
 
-			setProducts(data);
-		};
-		fetchProducts();
-	}, []);
-	return (
-		<>
-			<h1>Latest Products</h1>
-			<Row>
-				{products.map((product) => (
-					<Col className="align-items-stretch d-flex" 
-					key={product._id} sm={12} med={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
-		</>
-	);
-};
-
-export default HomeScreen;
+export default HomeScreen
