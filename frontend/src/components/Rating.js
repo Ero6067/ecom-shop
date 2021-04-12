67885a0dcf966afd8d12b34bfe50eb2383ec1d82
Kris Lafrance <kris.lafrance6067@gmail.com>
2021-04-12 01:57:31 -0700

    display front end product qty and add to cart button

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,82 +1,83 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 export const Rating = ({ value, text, color }) => {
-	return (
-		<div className='rating'>
-			<span>
-				<i
-					style={{ color }}
-					className={
-						value >= 1
-							? 'fas fa-star'
-							: value >= 0.5
-							? 'fas fa-star-half-alt'
-							: 'far fa-star'
-					}
-				/>
-			</span>
-			<span>
-				<i
-					style={{ color }}
-					className={
-						value >= 2
-							? 'fas fa-star'
-							: value >= 1.5
-							? 'fas fa-star-half-alt'
-							: 'far fa-star'
-					}
-				/>
-			</span>
-			<span>
-				<i
-					style={{ color }}
-					className={
-						value >= 3
-							? 'fas fa-star'
-							: value >= 2.5
-							? 'fas fa-star-half-alt'
-							: 'far fa-star'
-					}
-				/>
-			</span>
-			<span>
-				<i
-					style={{ color }}
-					className={
-						value >= 4
-							? 'fas fa-star'
-							: value >= 3.5
-							? 'fas fa-star-half-alt'
-							: 'far fa-star'
-					}
-				/>
-			</span>
-			<span>
-				<i
-					style={{ color }}
-					className={
-						value >= 5
-							? 'fas fa-star'
-							: value >= 4.5
-							? 'fas fa-star-half-alt'
-							: 'far fa-star'
-					}
-				/>
-			</span>
-			<div className='ml-2 '>{text && text}</div>
-		</div>
-	);
-};
+  return (
+    <div className="rating">
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 1
+              ? 'fas fa-star'
+              : value >= 0.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 2
+              ? 'fas fa-star'
+              : value >= 1.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 3
+              ? 'fas fa-star'
+              : value >= 2.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 4
+              ? 'fas fa-star'
+              : value >= 3.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 5
+              ? 'fas fa-star'
+              : value >= 4.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <div className="ml-2 ">{text && text}</div>
+    </div>
+  )
+}
 
 Rating.defaultProps = {
-	color: '#f8e825',
-};
+  color: '#f8e825',
+}
 
 Rating.propTypes = {
-	value: PropTypes.number.isRequired,
-	text: PropTypes.string.isRequired,
-	color: PropTypes.string,
-};
+  //ERROR: Failed prop type: The propr 'value' is marked as req in 'Rating', but its value is undefined
+  //   value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+}
 
-export default Rating;
+export default Rating
