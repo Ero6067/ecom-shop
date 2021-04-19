f344f0fc0d0a6ad3ecf996b562354ad59512aa02
Kris <kris.lafrance6067@gmail.com>
2021-04-18 18:49:12 -0700

    adding user authentication endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,7 @@ import Product from '../models/productModel.js'
 
 // @desc Fetch all products
 // @route GET /api/products
-// @access Publict
+// @access Public
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({})
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import {
+  notFound,
+  errorHandler,
+} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -12,11 +16,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-	res.send('API is running...');
-});
+  res.send('API is running...')
+})
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
@@ -25,6 +32,9 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 app.listen(
-	PORT, 
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      .yellow.bold,
+  ),
 )
